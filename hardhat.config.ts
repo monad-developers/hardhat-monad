28c61b2fe9cd4276b5e9f371abd0379efa407ba9
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-11-23 13:12:24 -0300

    Testnet verif + deployment work

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,7 +22,7 @@ const config: HardhatUserConfig = {
       chainId: 10143,
     },
     monadMainnet: {
-      url: `https://rpc.monad.xyz`,
+      url: "https://rpc.monad.xyz",
       accounts: [PRIVATE_KEY],
       chainId: 143,
     },
@@ -30,29 +30,29 @@ const config: HardhatUserConfig = {
   sourcify: {
     enabled: true,
     apiUrl: "https://sourcify-api-monad.blockvision.org",
-    browserUrl: "https://monadvision.com",
+    browserUrl: "https://testnet.monadvision.com",
   },
   etherscan: {
     enabled: true,
     apiKey: {
-      monadMainnet: ETHERSCAN_API_KEY,
       monadTestnet: ETHERSCAN_API_KEY,
+      monadMainnet: ETHERSCAN_API_KEY,
     },
     customChains: [
-      {
-        network: "monadMainnet",
-        chainId: 143,
-        urls: {
-          apiURL: "https://api.etherscan.io/v2/api?chainid=143",
-          browserURL: "https://monadscan.com",
-        },
-      },
       {
         network: "monadTestnet",
         chainId: 10143,
         urls: {
           apiURL: "https://api.etherscan.io/v2/api?chainid=10143",
           browserURL: "https://testnet.monadscan.com",
+        },
+      },
+      {
+        network: "monadMainnet",
+        chainId: 143,
+        urls: {
+          apiURL: "https://api.etherscan.io/v2/api?chainid=143",
+          browserURL: "https://monadscan.com",
         },
       },
     ],
